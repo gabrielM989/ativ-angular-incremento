2b9327a74c91bbc7002f706dcb8860ee0e5ca382
gabrielM989 <gabriel.marques9899@gmail.com>
2022-03-07 18:54:20 -0300

    atividade Angular Contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'atividadeAula22';
+
+incremento: number = 0
+
+  constructor() {} 
+ 
+  incrementar() {
+
+      this.incremento = this.incremento + 1;
+   
+  }
+  decrementar() {
+      this.incremento = this.incremento - 1;
+     
+  }
+
 }
+
+export class ButtonOverviewExample {}
